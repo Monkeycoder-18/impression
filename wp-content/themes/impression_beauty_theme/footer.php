92581285f855e9d6f6307c10e94bc60a4169ad28
Monkeycoder-18 <epsoncalorio@gmail.com>
2026-08-29 11:33:52 +0800

    Added test comment for webhook

--- wp-content/themes/impression_beauty_theme/footer.php
+++ wp-content/themes/impression_beauty_theme/footer.php
@@ -5,36 +5,35 @@
         <div class="row g-4">
 
             <div class="col-lg-4">
-                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
                     <img
-                        src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/logo.png' ); ?>"
-                        alt="<?php bloginfo( 'name' ); ?>"
-                        class="navbar-logo"
-                    >
+                        src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logo.png'); ?>"
+                        alt="<?php bloginfo('name'); ?>"
+                        class="navbar-logo">
                 </a>
                 <p class="text-muted mt-3 footer-tagline">
-                    <?php bloginfo( 'description' ); ?>
+                    <?php bloginfo('description'); ?>
                 </p>
             </div>
 
             <!-- <div class="col-lg-2">
                 <h5>Shop</h5>
                 <?php
-                wp_nav_menu( [
+                wp_nav_menu([
                     'theme_location' => 'footer',
                     'container'      => false,
                     'menu_class'     => '',
                     'fallback_cb'    => 'impression_beauty_footer_fallback',
-                ] );
+                ]);
                 ?>
             </div> -->
 
             <div class="col-lg-2">
                 <h5>Company</h5>
                 <ul>
-                    <li><a href="<?php echo esc_url( home_url( '/about' ) ); ?>">About</a></li>
-                    <li><a href="<?php echo esc_url( home_url( '/contact' ) ); ?>">Contact</a></li>
-                    <li><a href="<?php echo esc_url( home_url( '/blog' ) ); ?>">Blog</a></li>
+                    <li><a href="<?php echo esc_url(home_url('/about')); ?>">About</a></li>
+                    <li><a href="<?php echo esc_url(home_url('/contact')); ?>">Contact</a></li>
+                    <li><a href="<?php echo esc_url(home_url('/blog')); ?>">Blog</a></li>
                     <li><a href="#">Support</a></li>
                 </ul>
             </div>
@@ -42,7 +41,7 @@
             <div class="col-lg-2">
                 <h5>Legal</h5>
                 <ul>
-                    <li><a href="<?php echo esc_url( home_url( '/privacy-policy' ) ); ?>">Privacy Policy</a></li>
+                    <li><a href="<?php echo esc_url(home_url('/privacy-policy')); ?>">Privacy Policy</a></li>
                     <li><a href="#">Terms</a></li>
                     <li><a href="#">Shipping</a></li>
                     <li><a href="#">Returns</a></li>
@@ -62,7 +61,7 @@
         </div>
 
         <div class="copyright">
-            &copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>. All Rights Reserved.
+            &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All Rights Reserved.
         </div>
 
     </div>
@@ -70,4 +69,7 @@
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
+
+<!-- TEST COMMENT PULL REQUEST -->
